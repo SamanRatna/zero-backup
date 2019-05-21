234d84b06f9e7970488fb028595ea90ddd42a49e
Kanishka Suwal <suwalkanishka@gmail.com>
2019-05-21 12:26:06 +0545

    Experimented with maps using folium and webview. Webview allows viewing html in a Python window.

---
+++ maps_folium/display_modules_integration.py
@@ -0,0 +1,17 @@
+# import pandas as pd
+import folium
+import webview
+
+# Create map object
+folium_map = folium.Map(location=[27.7, 85.3],
+                        zoom_start=13,
+                        tiles="CartoDB dark_matter")
+
+# Create circle marker
+folium.CircleMarker(location=[27.7, 85.3], fill=True).add_to(folium_map)
+
+# Generate map
+folium_map.save('map.html')
+
+# Display Map
+webview.create_window('Map Time', 'map.html')
