c7387e831a38e3cc0bdc250188ab5f69ce3f4714
unknown <suwalkanishka@gmail.com>
2019-05-22 16:30:31 +0545

    Integrated graphs, image and a website in a single window. Maps isn't loading for unknown reasons; still working on it.

--- 002_map_folium_webview.py
+++ 002_map_folium_webview.py
@@ -11,7 +11,7 @@ folium_map = folium.Map(location=[27.7, 85.3],
 folium.CircleMarker(location=[27.7, 85.3], fill=True).add_to(folium_map)
 
 # Generate map
-folium_map.save('map.html')
+folium_map.save('901_map.html')
 
 # Display Map
-webview.create_window('Map Time', 'map.html')
+webview.create_window('Map Time', '901_map.html')
---
+++ A - archives/501_integrate_graph_map.py
@@ -0,0 +1,17 @@
+import sys
+from PyQt4 import QtGui
+
+
+class Window(QtGui.QMainWindow):
+    def __init__(self):
+        super(Window, self).__init__()
+        self.setGeometry(50, 50, 500, 300)
+        self.setWindowTitle("TITLE")
+        self.setWindowIcon(QtGui.QIcon('images/yatri_logo.png'))
+        self.show()
+
+
+
+app = QtGui.QApplication(sys.argv)
+GUI = Window()
+sys.exit(app.exec_())
