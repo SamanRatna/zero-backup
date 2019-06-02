198e1b08214c0315d0e8a0607af1170eac60175e
Ashim Pandey <maverick@Ashims-MacBook-Pro.local>
2019-06-02 12:23:49 +0545

    Implemented live graph w/ Python and JS running separately w/o server

--- 2 - Language Integration - Kanishka/Python JSON Live/Using AJAX - XMLHTTP/getdata.py
+++ 2 - Language Integration - Kanishka/Python JSON Live/Using AJAX - XMLHTTP/getdata.py
@@ -1,9 +1,11 @@
 import random
 import json
+import time
 
-# Create a random data point
-x_data = random.randint(1, 25)
-x_json = {'x': x_data}
+while True:
+    # Create a random data point
+    x_data = random.randint(1, 25)
+    x_json = {'x': x_data}
 
-with open('x_data.json', 'w') as file:
-    json.dump(x_json, file)
+    with open('x_data.json', 'w') as file:
+        json.dump(x_json, file)
