4bd636dad46a04a87c7e33be6cf8118f658e9462
unknown <suwalkanishka@gmail.com>
2019-06-10 11:21:50 +0545

    Minor time steps update.

--- 2 - Language Integration - Kanishka/Python JSON Live/Using AJAX - XMLHTTP/getdata.py
+++ 2 - Language Integration - Kanishka/Python JSON Live/Using AJAX - XMLHTTP/getdata.py
@@ -3,6 +3,9 @@ import json
 import time
 
 while True:
+    # Start time counter
+    start = time.time()
+
     # Create a random data point
     x_data = random.randint(1, 25)
     x_json = {'x': x_data}
@@ -10,4 +13,9 @@ while True:
     with open('x_data.json', 'w') as file:
         json.dump(x_json, file)
 
-    time.sleep(.05)
+    time.sleep(.01)
+
+    # End time counter and display time taken
+    end = time.time()
+    if end-start != 0:
+        print('Time taken: ', end-start, ' seconds')
