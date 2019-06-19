350d7b568242817e3a8afb8d2516d976d9fee50e
Ashesh Shrestha <asheshshrestha007@gmail.com>
2019-06-19 16:41:33 +0545

    Frontend stand by mode - except music

--- frontend/main.js
+++ frontend/main.js
@@ -422,3 +422,91 @@ jQuery(function ($) {
         $('.container').append('<p class="no-support">' + noSupport + '</p>');
     }
 });
+
+
+
+/* 
+
+
+
+
+
+
+
+above this - music
+
+now - topBar
+
+
+
+
+
+
+
+
+
+*/
+
+
+timeCount();
+function timeCount() {
+    var today = new Date();
+    
+    // var day = today.getDay();        /* generates 0-6 */
+    const day = today.toLocaleString('en-us', { weekday: 'long' });
+    var date = today.getDate();
+    // var month = today.getMonth()+1;  /* generates 0-11 */
+    const month = today.toLocaleString('en-us', { month: 'long' });
+    var year = today.getFullYear();
+
+    var hour = today.getHours();
+    if(hour<10)hour = "0"+hour;
+
+    var minute = today.getMinutes();
+    if(minute<10)minute = "0"+minute;
+
+    var second = today.getSeconds();
+    if(second<10)second = "0"+second;
+
+    // document.getElementById("clock").innerHTML = 
+    // day+"/"+month+"/"+year+" |"+hour+":"+minute+":"+second;
+
+    document.getElementById("time").innerHTML = 
+    hour+":"+minute+":"+second;
+
+    document.getElementById("date").innerHTML = 
+    day+","+date+" "+month+" "+year;
+
+    setTimeout("timeCount()", 10);
+}
+
+
+
+
+/* 
+
+
+
+
+
+
+
+above this - slider
+
+now - speed
+
+
+
+
+
+
+
+
+
+*/
+
+
+setInterval(function(){   
+    document.getElementById("speed").innerHTML=Math.floor((Math.random()*100)+1); 
+
+ }, 3000);
