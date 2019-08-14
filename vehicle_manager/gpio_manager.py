b7aa65cb2307cfa703f2efc3de7a4afebda40394
Sulav Lal Shrestha <ssulavshr@gmail.com>
2019-08-14 17:21:08 +0545

    [1] Splitted the class GPIOManager into GPIOReader and GPIOWriter [2] Refactored the methods of the classes [3] Introduced a class GPIOWriterMock for testing purpose

--- vehicle_manager/gpio_manager.py
+++ vehicle_manager/gpio_manager.py
@@ -1,14 +1,15 @@
 import json
 from periphery import GPIO
 from vehicle_states import *
+from event_handler import *
 
-class GPIOManager():
+class GPIOReader():
     __instance = None
     @staticmethod
     def getInstance():
-        if GPIOManager.__instance == None:
-            GPIOManager()
-        return GPIOManager.__instance
+        if GPIOReader.__instance == None:
+            GPIOReader()
+        return GPIOReader.__instance
 
     """
     __init__:
@@ -16,11 +17,20 @@ class GPIOManager():
         intializes the GPIOs
     """
     def __init__(self):
-        if GPIOManager.__instance != None:
-            raise Exception("GPIOManager is a Singleton Class.")
+        if GPIOReader.__instance != None:
+            raise Exception("GPIOReader is a Singleton Class.")
         else:
-            GPIOManager.__instance = self
+            GPIOReader.__instance = self
             self.pinState = {
+                            eGPIO.IN_HIBEAM:0,
+                            eGPIO.IN_LTURN:0,
+                            eGPIO.IN_RTURN:0,
+                            eGPIO.IN_BUTTON_RD:0,
+                            eGPIO.IN_BUTTON_RU:0,
+                            eGPIO.IN_BUTTON_RB:0,
+                            eGPIO.IN_STAND:0
+                        }
+            self.inputState = {
                             eGPIO.IN_HIBEAM:0,
                             eGPIO.IN_LTURN:0,
                             eGPIO.IN_RTURN:0,
@@ -46,6 +56,66 @@ class GPIOManager():
         self.in_button_ru = GPIO(config['in_button_ru']['pin'], config['in_button_ru']['direction'])
         self.in_button_rb = GPIO(config['in_button_rb']['pin'], config['in_button_rb']['direction'])
         self.in_stand = GPIO(config['in_stand']['pin'], config['in_stand']['direction'])
+
+    def monitorGPIO(self):
+        self.inputState[eGPIO.IN_HIBEAM] = self.in_hibeam.read()
+        self.inputState[eGPIO.IN_LTURN] = self.in_lturn.read()
+        self.inputState[eGPIO.IN_RTURN] = self.in_rturn.read()
+        self.inputState[eGPIO.IN_BUTTON_RD] = self.in_button_rd.read()
+        self.inputState[eGPIO.IN_BUTTON_RU] = self.in_button_ru.read()
+        self.inputState[eGPIO.IN_BUTTON_RB] = self.in_button_rb.read()
+        self.inputState[eGPIO.IN_STAND] = self.in_stand.read()
+
+        self.processInput(self.inputState)
+        self.pinState = self.inputState
+
+    def processInput(self, inputState):
+        inputChanges = []
+        for pin in inputState:
+            if inputState[pin] != self.pinState[pin]:
+                self.invokeEvent(pin, inputState[pin])
+
+    def invokeEvent(self, eventId, value):
+        if eventId == eGPIO.IN_HIBEAM:
+            vehicleEvents.onHibeamToggle(value)
+        elif eventId == eGPIO.IN_LTURN:
+            vehicleEvents.onLeftSideLightToggle(value)
+        elif eventId == eGPIO.IN_RTURN:
+            vehicleEvents.onRightSideLightToggle(value)
+        elif eventId == eGPIO.IN_BUTTON_RB:
+            vehicleEvents.onRBPress()
+        elif eventId == eGPIO.IN_BUTTON_RD:
+            vehicleEvents.onRDPress()
+        elif eventId == eGPIO.IN_BUTTON_RU:
+            vehicleEvents.onRUPress()
+        elif eventId == eGPIO.IN_STAND:
+            vehicleEvents.onStandSwitch()
+
+class GPIOWriter():
+    __instance = None
+    @staticmethod
+    def getInstance():
+        if GPIOWriter.__instance == None:
+            GPIOWriter()
+        return GPIOWriter.__instance
+    """
+    __init__:
+        initializes the variables/states
+        intializes the GPIOs
+    """
+    def __init__(self):
+        if GPIOWriter.__instance != None:
+            raise Exception("GPIOWriter is a Singleton Class.")
+        else:
+            GPIOWriter.__instance = self
+            #self.initializeGPIO('gpio_config.json')
+    """
+    initializeGPIO:
+        initializes the GPIOs with the help of config file provided as an argument
+    """
+    def initializeGPIO(self, gpio_config_file):
+        with open(gpio_config_file) as cfg_file:
+            config = json.load(cfg_file)
         
         self.out_start_thikka = GPIO(config['out_start_thikka']['pin'], config['out_start_thikka']['direction'])
         self.out_suste = GPIO(config['out_suste']['pin'], config['out_suste']['direction'])
@@ -56,30 +126,6 @@ class GPIOManager():
         self.out_lturn = GPIO(config['out_lturn']['pin'], config['out_lturn']['direction'])
         self.out_rturn = GPIO(config['out_rturn']['pin'], config['out_rturn']['direction'])
 
-    """
-    monitorPins:
-        reads the state of the pins
-        updates the pin_state member variable
-    """
-    def monitorGPIO(self):
-        inputState[eGPIO.IN_HIBEAM] = in_hibeam.read()
-        inputState[eGPIO.IN_LTURN] = in_lturn.read()
-        inputState[eGPIO.IN_RTURN] = in_rturn.read()
-        inputState[eGPIO.IN_BUTTON_RD] = in_button_rb.read()
-        inputState[eGPIO.IN_BUTTON_RU] = in_button_rb.read()
-        inputState[eGPIO.IN_BUTTON_RB] = in_button_rb.read()
-        inputState[eGPIO.IN_STAND] = in_stand.read()
-        
-        inputChanges = self.determineInputChange(inputState)
-        self.pinState = inputState
-        return inputChanges, inputState
-    """
-    determineInputChange:
-        finds out if the state has changed
-    """
-    def determineInputChange(self, inputState):
-        inputChanges = []
-        for pin in inputState:
-            if inputState[pin] != self.pinState[pin]:
-                inputChanges.append(pin)
-        return inputChanges
+class GPIOWriterMock:
+    def __init__(self):
+        print('Mocking GPIOWriter')
