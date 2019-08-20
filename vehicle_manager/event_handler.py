63480e1da99b8939d246be4fe67a7b88106de807
Sulav Lal Shrestha <ssulavshr@gmail.com>
2019-08-20 16:31:13 +0545

    Added print logs for all the events

--- vehicle_manager/event_handler.py
+++ vehicle_manager/event_handler.py
@@ -15,7 +15,27 @@ def eventRBPress():
 def eventRDPress():
     print("RD Press Event Triggered.")
 
+def eventLeftTurn():
+    print("Left Turn Toggled")
+
+def eventRightTurn():
+    print("Right Turn Toggled")
+
+def eventStand(state):
+    print("Stand Toggled: " + str(state))
+
+def eventBrake(state):
+    print("Brake Toggled: " + str(state))
+
+def eventHibeam(state):
+    print("Hibeam toggled: " + str(state))
+
 vehicleEvents.onRUPress += eventRUPress
 vehicleEvents.onRBPress += eventRBPress
 vehicleEvents.onRDPress += eventRDPress
+vehicleEvents.onHibeamToggle += eventHibeam
+vehicleEvents.onRightSideLightToggle += eventRightTurn
+vehicleEvents.onLeftSideLightToggle += eventLeftTurn
+vehicleEvents.onBrakeToggle += eventBrake
+vehicleEvents.onStandSwitch += eventSwitch
 
