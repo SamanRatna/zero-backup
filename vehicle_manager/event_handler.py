208bf1cebefac015d9923bfe1f0b8387a3cd9743
Sulav Lal Shrestha <ssulavshr@gmail.com>
2019-08-20 13:03:21 +0545

    Added some events and debugging features

--- vehicle_manager/event_handler.py
+++ vehicle_manager/event_handler.py
@@ -1,6 +1,21 @@
 from events import Events
 
 class VehicleEvents(Events):
-    __events__ = ('onRUPress','onRDPress', 'onRBPress', 'onHibeamToggle', 'onRightSideLightToggle', 'onLeftSideLightToggle', 'onBrakeToggle', 'onStandSwitch', 'onIgnition')
+    __events__ = ('onRUPress','onRDPress', 'onRBPress', 'onHibeamToggle', 'onRightSideLightToggle', 'onLeftSideLightToggle', 'onBrakeToggle', 'onStandSwitch', 'onIgnition', 'onCharging')
 
 vehicleEvents = VehicleEvents()
+
+
+def eventRUPress():
+    print("RU Press Event Triggered.")
+
+def eventRBPress():
+    print("RB Press Event Triggered.")
+
+def eventRDPress():
+    print("RD Press Event Triggered.")
+
+vehicleEvents.onRUPress += eventRUPress
+vehicleEvents.onRBPress += eventRBPress
+vehicleEvents.onRDPress += eventRDPress
+
