00ca0c48bc778766bb2920cb9ee007b3fd25ec3d
Sulav Lal Shrestha <ssulavshr@gmail.com>
2019-08-21 11:39:17 +0545

    Main file editing to incorporate more yatri modules

--- vehicle_manager/vm_main.py
+++ vehicle_manager/vm_main.py
@@ -1,15 +1,9 @@
 from event_handler import *
-
+from gpio_manager import *
 from state_manager import StateManager
+from tail_light_controller import TailLightController
 import vehicle_states
 
-"""
-def onRUPressEvent():
-    print(5)
-RUPressEvent = Events()
-RUPressEvent.on_change += onRUPressEvent
-RUPressEvent.on_change()
-"""
-#eventHandle = EventHandler.getInstance()
-stateMgr = StateManager.getInstance()
-vehicleEvents.onRUPress()
+stateMgr = StateManager.getInstance(GPIOWriter.getInstance())
+tlContoller = TailLightController(GPIOWriter.getInstance())
+gpioReader = GPIOReader.getInstance()
