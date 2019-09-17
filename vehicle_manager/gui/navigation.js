0cb076520ebe07972147e480bef82c67eda5a6ea
Sulav Lal Shrestha <ssulavshr@gmail.com>
2019-09-17 16:52:47 +0545

    Improved the animation of OSK

--- vehicle_manager/gui/navigation.js
+++ vehicle_manager/gui/navigation.js
@@ -20,67 +20,25 @@ function closeKeyboard(){
 }
 
 function toggleKeyboard(){
-    myKeyboard = document.getElementById("keyboard-2");
-    myForm = document.getElementById("navigation-form-1");
+    myStatus = document.getElementById("status-2");
+    myLogo = document.getElementById("yatri-logo-2");
     if(toggleKeyboard.state=='closed'){
         console.log("Opening Keyboard");
         if(myKeyboard != null){
-            document.getElementById("status-2").style.display ="none";
-            myKeyboard.style.display="block";
-            myForm.style.display ="block";
             toggleKeyboard.state = 'open';
+            myStatus.style.animation ="vanishingStatus 0.5s 1 ease-out forwards";
+            myLogo.style.boxShadow = "0px -4px 5px 0px rgba(219,219,219,1)";
         }
     }
     else if(toggleKeyboard.state=='open'){
         console.log("Closing Keyboard");
         if(myKeyboard != null){
-            document.getElementById("status-2").style.display ="block";
-            myForm.style.display ="none";
-            myKeyboard.style.display="none";
+            myStatus.style.animation ="appearingStatus 0.5s 1 ease-out forwards";
+            myStatus.style.display ="block";
             toggleKeyboard.state = 'closed';
+            myLogo.style.boxShadow = "none";
         }
     }
 }
 
 toggleKeyboard.state = 'closed';
-
-function openSearchDialog(){
-    // Get the modal
-    var modal = document.getElementById("search-mode-1");
-
-    // Get the button that opens the modal
-    var btn = document.getElementById("nav-button");
-
-    // Get the <span> element that closes the modal
-    var span = document.getElementsByClassName("close")[0];
-
-    // When the user clicks the button, open the modal 
-    btn.onclick = function() {
-      modal.style.display = "block";
-    }
-
-    // When the user clicks on <span> (x), close the modal
-    span.onclick = function() {
-        modal.style.display = "none";
-    }
-
-    // When the user clicks anywhere outside of the modal, close it
-    window.onclick = function(event) {
-      if (event.target == modal) {
-        modal.style.display = "none";
-      }
-    }
-}
-
-// function longPressNavigate(){
-//     var region = document.getElementById('yatri-logo-1');
-
-//     region.addEventListener('long-press', function(){
-//         console.log("Long Press Detected")
-//         myKeyboard = document.getElementById("keyboard-2");
-//         if(myKeyboard != null){
-//             document.getElementById("status-2").style.display ="none";
-//             myKeyboard.style.display="block";
-//         }
-//     });
-// }
