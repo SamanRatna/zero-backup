d54c8fec6d197722d5fc73e037572d0ce7f1ba3b
Sulav Lal Shrestha <ssulavshr@gmail.com>
2019-09-17 14:36:25 +0545

    On-Screen-Keyboard integrated

---
+++ vehicle_manager/gui/keyboard.js
@@ -0,0 +1,48 @@
+let Keyboard = window.SimpleKeyboard.default;
+
+let myKeyboard = new Keyboard({
+  onChange: input => onChange(input),
+  onKeyPress: button => onKeyPress(button),
+  mergeDisplay: true,
+  layoutName: "default",
+  layout: {
+    default: [
+      "q w e r t y u i o p",
+      "a s d f g h j k l",
+      "{shift} z x c v b n m {backspace}",
+      "{numbers} {space} {ent}"
+    ],
+    shift: [
+      "Q W E R T Y U I O P",
+      "A S D F G H J K L",
+      "{shift} Z X C V B N M {backspace}",
+      "{numbers} {space} {ent}"
+    ],
+    numbers: ["1 2 3", "4 5 6", "7 8 9", "{abc} 0 {backspace}"]
+  },
+  display: {
+    "{numbers}": "123",
+    "{ent}": "return",
+    "{escape}": "esc ⎋",
+    "{tab}": "tab ⇥",
+    "{backspace}": "⌫",
+    "{capslock}": "caps lock ⇪",
+    "{shift}": "⇧",
+    "{controlleft}": "ctrl ⌃",
+    "{controlright}": "ctrl ⌃",
+    "{altleft}": "alt ⌥",
+    "{altright}": "alt ⌥",
+    "{metaleft}": "cmd ⌘",
+    "{metaright}": "cmd ⌘",
+    "{abc}": "ABC"
+  }
+});
+
+function onChange(input) {
+  document.querySelector(".input").value = input;
+  console.log("Input changed", input);
+}
+
+function onKeyPress(button) {
+  console.log("Button pressed", button);
+}
