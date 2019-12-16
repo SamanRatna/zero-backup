b7fb65746e83b65ced25a3f9f3b5f9eb8f719352
Sulav Lal Shrestha <ssulavshr@gmail.com>
2019-12-16 13:57:58 +0545

    Adding Map Reload Feature
    Problem Description:
    Sometimes map does not load or loads incomplete, due to lack of internet connectivity or some other issue.
    
    Which is why, I'm adding the feature to reload the map.
    
    Currently, the map reloads when clicking on the yatri-logo at the bottom. Later a refresh button will be added.

---
+++ vehicle_manager/gui-revised/javascript/map-driver.js
@@ -0,0 +1,27 @@
+function loadScript(url, callback){
+
+    var script = document.createElement("script")
+    script.type = "text/javascript";
+
+    if (script.readyState){  //IE
+        script.onreadystatechange = function(){
+            if (script.readyState == "loaded" ||
+                    script.readyState == "complete"){
+                script.onreadystatechange = null;
+                callback();
+            }
+        };
+    } else {  //Others
+        script.onload = function(){
+            callback();
+        };
+    }
+
+    script.src = url;
+    document.getElementsByTagName("head")[0].appendChild(script);
+}
+
+function reloadMap(){
+    loadScript("https://maps.googleapis.com/maps/api/js?key=AIzaSyDcaohklnZiHr2YywkAJbwV9o3EAF5JVcs", initMap);
+}
+document.getElementById('yatri-logo').addEventListener('click', reloadMap);
