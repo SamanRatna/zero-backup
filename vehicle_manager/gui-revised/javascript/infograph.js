1787479872e3e7afaefb6c13cb434a96beb46fe3
Sulav Lal Shrestha <ssulavshr@gmail.com>
2020-08-26 14:48:04 +0545

    Updated Bluetooth Settings Pane

--- vehicle_manager/gui-revised/javascript/infograph.js
+++ vehicle_manager/gui-revised/javascript/infograph.js
@@ -1,9 +1,11 @@
+// toggle speed infograph unit km <-> mile
 document.getElementById('js-speed-unit-toggle').addEventListener('click', function(){
     document.getElementById('js-speed-label-km').classList.toggle('active');
     document.getElementById('js-speed-label-mile').classList.toggle('active');
     document.getElementById('js-speed-unit-button').classList.toggle('toggled');
 });
 
+// toggle temperature infograph unit Celsius <-> Farenheit
 document.getElementById('js-temperature-unit-toggle').addEventListener('click', function(){
     document.getElementById('js-temperature-label-c').classList.toggle('active');
     document.getElementById('js-temperature-label-f').classList.toggle('active');
