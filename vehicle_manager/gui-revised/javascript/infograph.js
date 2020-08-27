682c5d17b6479d7caf8995ef4150dcc6357b6b90
Sulav Lal Shrestha <ssulavshr@gmail.com>
2020-08-27 09:30:28 +0545

    Added Brightness Control

--- vehicle_manager/gui-revised/javascript/infograph.js
+++ vehicle_manager/gui-revised/javascript/infograph.js
@@ -11,3 +11,78 @@ document.getElementById('js-temperature-unit-toggle').addEventListener('click', 
     document.getElementById('js-temperature-label-f').classList.toggle('active');
     document.getElementById('js-temperature-unit-button').classList.toggle('toggled');
 });
+
+// brightness control
+let slider = document.getElementById('js-brightness-slider');
+slider.oninput = function() {
+    let x = slider.value * 10 - 5;
+    let color = 'linear-gradient(90deg, #30D5C8 ' + x + '%, #EAEAEA ' + x + '%)';
+    slider.style.background = color;
+    eel.changeBrightness(slider.value);
+}
+
+// Connectivity Settings
+document.getElementById('js-settings-connectivity').addEventListener('click', function(){
+    document.getElementById('js-settings-connectivity').classList.toggle('settings-connectivity-selected');
+    document.getElementById('js-connectivity-pane').classList.toggle('right-pane-visible');
+    document.getElementById('js-infographics-pane').classList.toggle('right-pane-visible');
+});
+
+// Bluetooth
+function requestBluetoothPairingConfirmation(passkey){
+    document.getElementById('js-bluetooth-passkey').innerHTML = passkey;
+    setBluetoothNotificationVisibility(true);
+}
+
+// accept bluetooth pairing
+document.getElementById('js-ble-button-yes').addEventListener('click',function(){
+    setBluetoothNotificationVisibility(true);
+    respondBluetoothPairing('yes');  
+});
+
+// reject bluetooth pairing
+document.getElementById('js-ble-button-no').addEventListener('click',function(){
+    setBluetoothNotificationVisibility(false);
+    respondBluetoothPairing('no');  
+});
+
+// bluetooth on-off toggle
+document.getElementById('js-bluetooth-toggle').addEventListener('click', function(){
+    document.getElementById('js-bluetooth-label-on').classList.toggle('active');
+    document.getElementById('js-bluetooth-label-off').classList.toggle('active');
+    document.getElementById('js-bluetooth-toggle-button').classList.toggle('toggled');
+    
+    if(document.getElementById('js-bluetooth-label-on').classList.contains('active')){
+        eel.changeBluetoothState(true);
+    }
+    else{
+        eel.changeBluetoothState(false);
+    }
+});
+
+// update the name and status of th device connected to bluetooth
+function updateBluetoothStatus(name, status){
+    console.log("Bluetooth Status: " + name + " : " +status)
+    if(status == '1'){
+        document.getElementById('js-bluetooth-device').innerHTML = name;
+    }
+    else if(status == '0'){
+        document.getElementById('js-bluetooth-device').innerHTML = ' ';
+    }
+}
+
+function updateAdvertisementStatus(status){
+    console.log("Bluetooth Advertisement Status: " + status)
+    if(status =='1'){
+        document.getElementById('js-bluetooth-label-on').classList.add('active');
+        document.getElementById('js-bluetooth-label-off').classList.remove('active');
+        document.getElementById('js-bluetooth-toggle-button').classList.add('toggled');
+        document.getElementById('js-bluetooth-status').innerHTML = 'Discoverable';
+    }
+    else if(status == '0'){
+        document.getElementById('js-bluetooth-label-on').classList.remove('active');
+        document.getElementById('js-bluetooth-label-off').classList.add('active');
+        document.getElementById('js-bluetooth-toggle-button').classList.remove('toggled');
+        document.getElementById('js-bluetooth-status').innerHTML = 'Hidden';
+    }
+}
