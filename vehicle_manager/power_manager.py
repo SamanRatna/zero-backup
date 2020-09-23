0d66c1c38d5fd63892113ab1880192bbefb76bc5
Sulav Lal Shrestha <ssulavshr@gmail.com>
2020-09-23 16:25:24 +0545

    Handling of the display on bike On and Off

--- vehicle_manager/power_manager.py
+++ vehicle_manager/power_manager.py
@@ -1,13 +1,15 @@
 from event_handler import *
 import threading
 from gpio_manager import RepeatableTimer
-
+import os
 
 class PowerManager():
     def __init__(self):
         self.standState = 0
         vehicleEvents.onStandSwitch += self.updateStandState
         vehicleEvents.onUserInteraction += self.uiMonitor
+        vehicleEvents.bikeOff += self.onBikeOff
+        vehicleEvents.bikeOn += self.onBikeOn
         # self.inactivityTimer = threading.Timer(5.0, self.poweroff)
         self.inactivityTimer = RepeatableTimer(10.0, self.poweroff)
     def updateStandState(self, state):
@@ -28,3 +30,11 @@ class PowerManager():
         if(self.standState == 1 and state == 1):
             self.inactivityTimer.cancel()
             self.inactivityTimer.start()
+
+    def onBikeOff(self):
+        os.system('tvservice -o')
+        os.system('systemctl stop guix.service')
+
+    def onBikeOn(self):
+        os.system('tvservice -p')
+        os.system('systemctl start guix.service')
