75fe945b29b89f70a325b5efc6dd826a50b1a41e
Sulav Lal Shrestha <ssulavshr@gmail.com>
2021-01-29 18:07:53 +0545

    Script file change

--- scripts/1.sh
+++ scripts/1.sh
@@ -1,5 +1,6 @@
-# sudo ip link set can0 up type can bitrate 500000
-sudo btmgmt -i hci0 bredr off
-sudo btmgmt -i hci0 le on
+ip link set can0 up type can bitrate 500000 &
+# sudo btmgmt -i hci0 bredr off
+# sudo btmgmt -i hci0 le on
+pon &
 cd /home/pi/yatri-project-zero/vehicle_manager
 python3 vm_main.py
